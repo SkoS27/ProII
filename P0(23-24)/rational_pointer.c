11693fd9d1e96e019ae58acfb5d7ad98a811dc72
Carmelo Alejandro García Almeida <115218862+SkoS27@users.noreply.github.com>
2023-02-03 16:37:10 +0100

    Add files via upload

---
+++ P0(23-24)/rational_pointer.c
@@ -0,0 +1,26 @@
+//
+// Created by Carmelo on 03/02/2023.
+//
+#include <stdlib.h>
+#include "rational_pointer.h"
+
+Rational createRational(int n, int d) {
+    Rational temp;
+    temp= malloc(sizeof(*temp));
+    temp->num= n;
+    temp->den= d;
+    return temp;
+}
+int numerator(Rational r) {
+    return r->num;
+}
+int denominator(Rational r) {
+    return r->den;
+}
+Rational sum(Rational r1, Rational r2) {
+    Rational s;
+    s = malloc(sizeof(*s));
+    s->num = r1->num * r2->den + r2->num * r1->den;
+    s->den = r1->den * r2->den;
+    return s;
+}
---
+++ P0(23-24)/rational_struct.h
@@ -0,0 +1,18 @@
+//
+// Created by Carmelo on 03/02/2023.
+//
+
+#ifndef PROII_P0_RATIONAL_STRUCT_H
+#define PROII_P0_RATIONAL_STRUCT_H
+
+typedef struct Data Rational;
+struct Data {
+    int num;
+    int den;
+};
+Rational createRational(int n, int d);
+int numerator(Rational r);
+int denominator(Rational r);
+Rational sum(Rational r1, Rational r2);
+
+#endif //PROII_P0_RATIONAL_STRUCT_H
